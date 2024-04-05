9d671d566d2d57a15ab3899d62bead04a5067662
fershoo101 <146652466+fersho101@users.noreply.github.com>
2024-04-05 11:27:49 -0700

    :sparkles: Agregando resumen de calorias

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from './components/Form'
 import { useReducer, useEffect, useMemo } from 'react'
 import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
+import CalorieTracker from './components/CalorieTracker'
 
 function App() {
 	const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -15,7 +16,7 @@ function App() {
 	return (
 		<>
 			<header className='bg-lime-600 py-3'>
-				<div className='max-w-4xl mx-auto flex justify-between'>
+				<div className='max-w-4xl mx-auto flex justify-between items-center'>
 					<h1 className='text-center text-lg font-bold text-white uppercase '>
 						Contador de calorias
 					</h1>
@@ -39,6 +40,14 @@ function App() {
 					/>
 				</div>
 			</section>
+			<section className='bg-gray-800 py-10'>
+				<div className="max-w-4xl mx-auto">
+					<CalorieTracker 
+						activities = {state.activities}
+					/>
+				</div>
+			</section>
+
 			<section className='p-10 mx-auto max-w-4xl '>
 				<ActivityList
 					activities={state.activities}
