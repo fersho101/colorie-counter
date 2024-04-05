1e440450be69bc7878c8a1e14a6c51052460c910
fershoo101 <146652466+fersho101@users.noreply.github.com>
2024-04-04 17:39:44 -0700

    Se agregan icono y funcionalidades

---
+++ src/components/ActivityList.tsx
@@ -0,0 +1,48 @@
+import { Activity } from '../types'
+import { categories } from '../data/categories'
+import { useMemo, Dispatch } from 'react'
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { ActivityActions } from '../reducers/activity-reducer'
+
+type ActivityListProps = {
+	activities: Activity[], 
+	dispatch: Dispatch<ActivityActions>
+}
+
+export default function ActivityList({ activities, dispatch }: ActivityListProps) {
+	const categoryName = useMemo(
+		() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''),
+		[activities]
+	)
+
+	return (
+		<>
+			<h2 className='text-4xl font-bold text-slate-600 text-center'>
+				Comida y Actividades
+			</h2>
+			{activities.map(elem => (
+				<div
+					key={elem.id}
+					className='px-5 py-10 bg-white mt-5 flex justify-between'
+				>
+					<div className='space-y-2 relative'>
+						<p className= {`absolute -top-8 -left-8 px-10 py-2 text-white mt-5 uppercase font-bold ${elem.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`} >{categoryName(+elem.category)}</p>
+						<p className=' text-2xl font-bold pt-5'>{elem.name}</p>
+						<p className=' font-black text-4xl text-lime-500'>
+							{elem.calories} <span>Calorias</span>
+						</p>
+					</div>
+					<div className='flex gap-5 items-center' >
+						<button
+							onClick={() => dispatch({type:"set-activeId", payload: {id:elem.id}})}
+						>
+							<PencilSquareIcon
+								className='h-8 w-8 text-gray-800'
+							/>
+						</button>
+					</div>
+				</div>
+			))}
+		</>
+	)
+}
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,26 +1,33 @@
 import { Activity } from '../types'
 
-export type ActivityActions = {
-	type: 'save-activity'
-	payload: { newActivity: Activity }
-}
+export type ActivityActions =
+	| { type: 'save-activity'; payload: { newActivity: Activity } }
+	| { type: 'set-activeId'; payload: { id: Activity['id'] } }
 
 type ActivityState = {
 	activities: Activity[]
+	activeId: Activity['id']
 }
 export const initialState: ActivityState = {
 	activities: [],
+	activeId: '',
 }
 
 export const activityReducer = (
 	state: ActivityState = initialState,
 	action: ActivityActions
 ) => {
-	if(action.type === 'save-activity') {
-		
+	if (action.type === 'save-activity') {
 		return {
 			...state,
-			activities: [...state.activities, action.payload.newActivity]
+			activities: [...state.activities, action.payload.newActivity],
+		}
+	}
+
+	if (action.type === 'set-activeId') {
+		return {
+			...state,
+			activeId: action.payload.id,
 		}
 	}
 
